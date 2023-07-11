b6e7dc4da0ffc5295dc5c7672534afa962c8ef3b
Timmy Tompkins <timhageman@Clementine.lan>
2023-07-11 17:28:38 -0500

    ready to deploy backend

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,5 @@
-const express = require('express');
-const flockRoutes = require('./routes/flock');
-const costRoutes = require('./routes/cost');
-const applyMiddleware = require('./middleware/middlewar');
-const app = express();
+const app = require('./server');
 const port = process.env.PORT || 3001;
-
-applyMiddleware(app);
-
-app.use('/flock', flockRoutes);
-app.use('/cost', costRoutes);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
